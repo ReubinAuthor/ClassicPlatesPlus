0ea5610accfb566fc8669fc43ab04e4c700b1cfa
Reubin <classicrooben@gmail.com>
2022-11-06 02:10:53 +0300

    2.0.0 Release

--- ReubinsNameplates/init.lua
+++ ReubinsNameplates/init.lua
@@ -1,9 +1,9 @@
 ----------------------------------------
--- NAMESPACES
+-- CORE
 ----------------------------------------
 local myAddon, core = ...;
 local func = core.func;
-local frames = core.frames;
+local data = core.data;
 
 ----------------------------------------
 -- HANDLING EVENTS 
@@ -11,90 +11,107 @@ local frames = core.frames;
 function core:init(event, ...)
     local arg = ...
 
-    -- Tweaking CVars
     if event == "VARIABLES_LOADED" then
-        SetCVar("nameplateSelectedScale", 1.15);
-        SetCVar("nameplateTargetRadialPosition", 1);
-        SetCVar("clampTargetNameplateToScreen", 1);
+        func:CVars(event);
     end
-    ----------------------------------------
     if event == "ADDON_LOADED" then
         if arg == myAddon then
             func:Load_Settings();
-            --func:Hide_Blizzard();
         end
     end
-    ----------------------------------------
     if event == "PLAYER_LOGOUT" then
         func:Save_Settings();
-
-        --Restoring CVars on logout to avoid poluting CVars in case user decides to uninstall addon
-        SetCVar("nameplateSelectedScale", 1);
-        SetCVar("nameplateTargetRadialPosition", 0);
-        SetCVar("clampTargetNameplateToScreen", 0);
+        func:CVars(event);
     end
-    ----------------------------------------
-    --[[
     if event == "NAME_PLATE_CREATED" then
-        func:Create_Nameplate(arg);
+        func:Nameplate_Created(arg);
     end
-    ]]
-    ----------------------------------------
     if event == "NAME_PLATE_UNIT_ADDED" then
-        func:Add_Nameplate(arg);
+        func:Nameplate_Added(arg);
     end
-
     if event == "NAME_PLATE_UNIT_REMOVED" then
-        func:Remove_Nameplate(arg);
+        func:Nameplate_Removed(arg);
     end
-    ----------------------------------------
     if event == "UNIT_THREAT_LIST_UPDATE" then
-        func:Update_Threat(arg);
+        func:Update_ThreatPercentage(arg);
+        func:Update_ThreatIcon(arg);
     end
-    ----------------------------------------
+    if event == "UNIT_THREAT_SITUATION_UPDATE" then
+        func:Update_ThreatIcon(arg);
+    end
+    if event == "UNIT_CLASSIFICATION_CHANGED" then
+        func:Update_Classification(arg);
+    end
     if event == "UNIT_HEALTH_FREQUENT" then
         func:Update_Health(arg);
     end
-    ----------------------------------------
+    if event == "UNIT_POWER_FREQUENT" then
+        func:Update_Powerbar(arg);
+    end
     if event == "UNIT_AURA" then
         func:Update_Auras(arg);
     end
-    ----------------------------------------
-    if event == "PLAYER_REGEN_ENABLED" then
-        frames.combat = false;
-        for k, v in pairs(frames.threat) do
-            if k then
-                v:SetShown(func:Threat_Toggle());
-            end
-        end
-    end
-    ----------------------------------------
-    if event == "PLAYER_REGEN_DISABLED" then
-        frames.combat = true;
-        for k, v in pairs(frames.threat) do
-            if k and UnitExists(k) then
-                v:SetShown(func:Threat_Toggle());
-            end
-        end
-    end
-    ----------------------------------------
     if event == "GROUP_ROSTER_UPDATE" then
-        func:Roster_Update();
-        for k, v in pairs(frames.threat) do
-            if k then
-                func:Update_Threat(k);
-            end
-        end
-
-        for k, v in pairs(frames.health) do
-            if k then
-                func:Update_Health(k);
-            end
-        end
+        func:Update_Roster();
     end
-    ----------------------------------------
     if event == "PLAYER_ENTERING_WORLD" then
-        func:Roster_Update();
+        func:Update_Roster();
+    end
+    if event == "PLAYER_TARGET_CHANGED" then
+        func:Selected();
+    end
+    if event == "UPDATE_MOUSEOVER_UNIT" then
+        func:Mouseover();
+    end
+    if event == "PLAYER_FLAGS_CHANGED" then
+        func:Update_healthbar(arg);
+    end
+    if event == "UNIT_FACTION" then
+        func:Update_healthbar(arg);
+        func:Update_Portrait(arg);
+        func:Update_PVP_Flag(arg);
+    end
+    if event == "UNIT_PORTRAIT_UPDATE" then
+        func:Update_Portrait(arg);
+        func:Update_Powerbar(arg);
+    end
+    if event == "UNIT_NAME_UPDATE" then
+        func:Update_Name(arg);
+    end
+    if event == "UNIT_LEVEL" then
+        func:Update_Level(arg);
+    end
+    if event == "UNIT_COMBAT" then
+        func:Update_Name(arg);
+        func:Update_healthbar(arg);
+    end
+    if event == "UNIT_MAXHEALTH" then
+        func:Update_Health(arg);
+        func:Update_healthbar(arg);
+    end
+    if event == "UNIT_MAXPOWER" then
+        func:Update_Powerbar(arg);
+    end
+    if event == "MODIFIER_STATE_CHANGED" then
+        local key, down = ...
+        func:Hotkeys_Pressed(key, down);
+    end
+    if event == "UNIT_SPELLCAST_START"
+    or event == "UNIT_SPELLCAST_CHANNEL_START"
+    or event == "UNIT_SPELLCAST_DELAYED"
+    or event == "UNIT_SPELLCAST_CHANNEL_UPDATE" then
+        func:Castbar_Start(arg);
+    end
+    if event == "UNIT_SPELLCAST_STOP"
+    or event == "UNIT_SPELLCAST_CHANNEL_STOP"
+    or event == "UNIT_SPELLCAST_FAILED"
+    or event == "UNIT_SPELLCAST_FAILED_QUIET"
+    or event == "UNIT_SPELLCAST_INTERRUPTED"
+    or event == "UNIT_SPELLCAST_SUCCEEDED" then
+        func:Castbar_End(event, arg);
+    end
+    if event == "RAID_TARGET_UPDATE" then
+        func:RaidTargetIndex();
     end
 end
 
@@ -105,33 +122,27 @@ SLASH_RPLATES1 = "/rplates";
 
 local function Slash_Handler(msg)
     if msg == "tank" then
-        ReubinsNameplates_settings.Tank = ReubinsNameplates_settings.Tank or false;
-        ReubinsNameplates_settings.Tank = not ReubinsNameplates_settings.Tank;
+        local nameplates = C_NamePlate.GetNamePlates();
 
-        -- Applying Threat visibility & Tank mode settings
-        for k, v in pairs(frames.threatIcon) do
-            if k then
-                if not ReubinsNameplates_settings.Tank then
-                    v:SetTexture("Interface\\addons\\ReubinsNameplates\\media\\aggro");
-                else
-                    v:SetTexture("Interface\\addons\\ReubinsNameplates\\media\\tanking");
+        ReubinsNameplates_settings.TankMode = ReubinsNameplates_settings.TankMode or false;
+        ReubinsNameplates_settings.TankMode = not ReubinsNameplates_settings.TankMode;
+
+        if nameplates then
+            for k,v in pairs(nameplates) do
+                if k then
+                    func:Update_ThreatIcon(v.unitFrame.unit);
                 end
-                func:Update_Threat(k);
             end
         end
 
-        -- Applying Threat visibility & Tank mode settings
-        for k, v in pairs(frames.threat_num) do
-            if k then
-                v:SetPoint(func:posThreatValue(k, C_NamePlate.GetNamePlateForUnit(k)));
-            end
-        end
-
-        if ReubinsNameplates_settings.Tank == true then
-            print("|cff00ccffReubin's Nameplates: |cffFFFC54Tank mode activated");
+        -- Typing out confirmation in the chat
+        if ReubinsNameplates_settings.TankMode then
+            print("|cfff563ff[Reubin's Nameplates] |cffffffffTank mode activated!");
         else
-            print("|cff00ccffReubin's Nameplates: |cffFFFC54Tank mode deactivated");
+            print("|cfff563ff[Reubin's Nameplates] |cffffffffTank mode deactivated!");
         end
+
+        data.TankModeCheckButton:SetChecked(ReubinsNameplates_settings.TankMode);
     end
 end
 
@@ -142,19 +153,48 @@ SlashCmdList["RPLATES"] = Slash_Handler;
 ----------------------------------------
 local events = CreateFrame("Frame");
 
-events:RegisterEvent("VARIABLES_LOADED");
+-- Events
 events:RegisterEvent("ADDON_LOADED");
-events:RegisterEvent("PLAYER_LOGOUT");
+events:RegisterEvent("VARIABLES_LOADED");
+events:RegisterEvent("NAME_PLATE_CREATED");
 events:RegisterEvent("NAME_PLATE_UNIT_ADDED");
 events:RegisterEvent("NAME_PLATE_UNIT_REMOVED");
-events:RegisterEvent("UNIT_THREAT_LIST_UPDATE");
-events:RegisterEvent("UNIT_HEALTH_FREQUENT");
-events:RegisterEvent("UNIT_AURA");
+events:RegisterEvent("PLAYER_LOGOUT");
+events:RegisterEvent("PLAYER_ENTERING_WORLD");
 events:RegisterEvent("PLAYER_REGEN_ENABLED");
 events:RegisterEvent("PLAYER_REGEN_DISABLED");
+events:RegisterEvent("PLAYER_FLAGS_CHANGED");
+events:RegisterEvent("PLAYER_TARGET_CHANGED");
+events:RegisterEvent("UNIT_THREAT_LIST_UPDATE");
+events:RegisterEvent("UNIT_THREAT_SITUATION_UPDATE");
+events:RegisterEvent("UNIT_CLASSIFICATION_CHANGED");
+events:RegisterEvent("UNIT_HEALTH_FREQUENT");
+events:RegisterEvent("UNIT_POWER_FREQUENT");
+events:RegisterEvent("UNIT_AURA");
+events:RegisterEvent("UNIT_FACTION");
+events:RegisterEvent("UNIT_PORTRAIT_UPDATE");
+events:RegisterEvent("UNIT_NAME_UPDATE");
+events:RegisterEvent("UNIT_LEVEL");
+events:RegisterEvent("UNIT_COMBAT");
+events:RegisterEvent("UNIT_MAXHEALTH");
+events:RegisterEvent("UNIT_MAXPOWER");
 events:RegisterEvent("GROUP_ROSTER_UPDATE");
-events:RegisterEvent("PLAYER_ENTERING_WORLD");
---events:RegisterEvent("PLAYER_TARGET_CHANGED");
---events:RegisterEvent("NAME_PLATE_CREATED");
+events:RegisterEvent("MODIFIER_STATE_CHANGED");
+events:RegisterEvent("UPDATE_MOUSEOVER_UNIT");
+events:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+events:RegisterEvent("RAID_TARGET_UPDATE");
 
+-- Castbar events
+events:RegisterEvent("UNIT_SPELLCAST_START");
+events:RegisterEvent("UNIT_SPELLCAST_CHANNEL_START");
+events:RegisterEvent("UNIT_SPELLCAST_DELAYED");
+events:RegisterEvent("UNIT_SPELLCAST_CHANNEL_UPDATE");
+events:RegisterEvent("UNIT_SPELLCAST_STOP");
+events:RegisterEvent("UNIT_SPELLCAST_CHANNEL_STOP");
+events:RegisterEvent("UNIT_SPELLCAST_FAILED");
+events:RegisterEvent("UNIT_SPELLCAST_FAILED_QUIET");
+events:RegisterEvent("UNIT_SPELLCAST_INTERRUPTED");
+events:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED");
+
+-- Scripts
 events:SetScript("OnEvent", core.init);
---
+++ ReubinsNameplates/threat.lua
@@ -0,0 +1,213 @@
+----------------------------------------
+-- CORE
+----------------------------------------
+local myAddon, core = ...;
+local func = core.func;
+local data = core.data;
+
+----------------------------------------
+-- Threat icon
+----------------------------------------
+function func:Update_ThreatIcon(unit)
+    if unit then
+        local nameplate = C_NamePlate.GetNamePlateForUnit(unit);
+
+        if nameplate then
+            local unitFrame = nameplate.unitFrame;
+            local frame = unitFrame.threat.icon;
+            local toggle;
+
+            if ReubinsNameplates_settings.ThreatIcon == "Always" then
+                toggle = true;
+            elseif ReubinsNameplates_settings.ThreatIcon == "Party & raid" then
+                if IsInRaid() or IsInGroup() then
+                    toggle = true;
+                else
+                    toggle = false;
+                end
+            elseif ReubinsNameplates_settings.ThreatIcon == "Never" then
+                toggle = false;
+            end
+
+            if toggle then
+                local notPlayerOrPet = not UnitIsPlayer(unit) and not UnitIsOtherPlayersPet(unit);
+                local icon = unitFrame.threat.icon.texture;
+                local animation = unitFrame.threat.animation;
+
+                -- Prepping for a color switch check
+                unitFrame.threat.color = unitFrame.threat.color or nil;
+                unitFrame.threat.colorPrev = unitFrame.threat.color;
+
+                if UnitCanAttack("player", unit) and notPlayerOrPet then
+
+                    -- Tank mode Activated
+                    if ReubinsNameplates_settings.TankMode then
+                        local isTanking = UnitDetailedThreatSituation("player", unit)
+
+                        -- Swapping icon
+                        icon:SetTexture("Interface\\addons\\ReubinsNameplates\\media\\icons\\tank");
+
+                        if isTanking then
+                            icon:SetVertexColor(data.colors.green.r, data.colors.green.g, data.colors.green.b);
+                            unitFrame.threat.color = "green";
+                            frame:SetShown(toggle);
+                        -- Target is anoter Tank
+                        elseif data.tanks[UnitName(unit .. "target")] then
+                            icon:SetVertexColor(data.colors.blue.r, data.colors.blue.g, data.colors.blue.b);
+                            unitFrame.threat.color = "blue";
+                            frame:SetShown(toggle);
+                        elseif UnitAffectingCombat(unit) then
+                            icon:SetVertexColor(data.colors.red.r, data.colors.red.g, data.colors.red.b);
+                            unitFrame.threat.color = "red";
+                            frame:SetShown(toggle);
+                        else
+                            frame:Hide();
+                        end
+
+                    -- Tank mode Deactivated
+                    else
+                        local treatPercentage = UnitThreatPercentageOfLead("player",unit)
+
+                        -- Swapping icon
+                        icon:SetTexture("Interface\\addons\\ReubinsNameplates\\media\\icons\\aggro");
+
+                        if treatPercentage then
+                            if treatPercentage >= 100 then
+                                icon:SetVertexColor(data.colors.red.r, data.colors.red.g, data.colors.red.b);
+                                unitFrame.threat.color = "red";
+                                frame:SetShown(toggle);
+                            elseif treatPercentage > 75 then
+                                icon:SetVertexColor(data.colors.orange.r, data.colors.orange.g, data.colors.orange.b);
+                                unitFrame.threat.color = "orange";
+                                frame:SetShown(toggle);
+                            elseif treatPercentage > 50 then
+                                icon:SetVertexColor(data.colors.yellow.r, data.colors.yellow.g, data.colors.yellow.b);
+                                unitFrame.threat.color = "yellow";
+                                frame:SetShown(toggle);
+                            else
+                                frame:Hide();
+                            end
+                        else
+                            frame:Hide();
+                        end
+                    end
+                else
+                    frame:Hide();
+                end
+
+                -- Play icon's animation on color change
+                if unitFrame.threat.colorPrev ~= unitFrame.threat.color then
+                    if animation:IsPlaying() then
+                        animation:Restart();
+                    else
+                        animation:Play();
+                    end
+                end
+            else
+                frame:Hide();
+            end
+        end
+    end
+end
+
+----------------------------------------
+-- Threat percentage
+----------------------------------------
+function func:Update_ThreatPercentage(unit)
+    if unit then
+        local nameplate = C_NamePlate.GetNamePlateForUnit(unit);
+
+        if nameplate then
+            local unitFrame = nameplate.unitFrame;
+
+            if ReubinsNameplates_settings.ThreatPercentage then
+                local percentage = unitFrame.threat.percentage;
+                local text = unitFrame.threat.percentage.text;
+                local background = unitFrame.threat.percentage.background;
+                local treatPercentage = UnitThreatPercentageOfLead("player",unit)
+
+                if treatPercentage and treatPercentage > 0 then
+                    if treatPercentage > 999 then
+                        text:SetText(">999%");
+                    else
+                        text:SetText(math.floor(treatPercentage) .. "%");
+                    end
+
+                    if ReubinsNameplates_settings.TankMode then
+                        if treatPercentage >= 200 then
+                            background:SetVertexColor(0, 1, 0); -- 1 Green
+                        elseif treatPercentage < 100 then
+                            background:SetVertexColor(1, 0, 0); -- 12 Red
+                        elseif treatPercentage < 110 then
+                            background:SetVertexColor(1, 0.23, 0); -- 11
+                        elseif treatPercentage < 120 then
+                            background:SetVertexColor(1, 0.34, 0); -- 10
+                        elseif treatPercentage < 130 then
+                            background:SetVertexColor(1, 0.44, 0); -- 9
+                        elseif treatPercentage < 140 then
+                            background:SetVertexColor(1, 0.55, 0); -- 8
+                        elseif treatPercentage < 150 then
+                            background:SetVertexColor(1, 0.69, 0); -- 7
+                        elseif treatPercentage < 160 then
+                            background:SetVertexColor(1, 0.83, 0); -- 6
+                        elseif treatPercentage < 170 then
+                            background:SetVertexColor(1, 0.95, 0); -- 5
+                        elseif treatPercentage < 180 then
+                            background:SetVertexColor(0.94, 1, 0); -- 4
+                        elseif treatPercentage < 190 then
+                            background:SetVertexColor(0.79, 1, 0); -- 3
+                        elseif treatPercentage < 200 then
+                            background:SetVertexColor(0.56, 1, 0); -- 2
+                        end
+                    else
+                        if treatPercentage >= 100 then
+                            background:SetVertexColor(1, 0, 0); -- 10 Red
+                        elseif treatPercentage >= 90 then
+                            background:SetVertexColor(1, 0.27, 0); -- 9
+                        elseif treatPercentage >= 80 then
+                            background:SetVertexColor(1, 0.39, 0); -- 8
+                        elseif treatPercentage >= 70 then
+                            background:SetVertexColor(1, 0.53, 0); -- 7
+                        elseif treatPercentage >= 60 then
+                            background:SetVertexColor(1, 0.67, 0); -- 6
+                        elseif treatPercentage >= 50 then
+                            background:SetVertexColor(1, 0.84, 0); -- 5
+                        elseif treatPercentage >= 40 then
+                            background:SetVertexColor(1, 0.98, 0); -- 4
+                        elseif treatPercentage >= 30 then
+                            background:SetVertexColor(0.88, 1, 0); -- 3
+                        elseif treatPercentage >= 20 then
+                            background:SetVertexColor(0.63, 1, 0); -- 2
+                        elseif treatPercentage >= 10 then
+                            background:SetVertexColor(0, 1, 0); -- 1
+                        elseif treatPercentage >= 0 then
+                            background:SetVertexColor(0, 1, 0); -- 0 Green
+                        end
+                    end
+
+                    -- Shifting unit's name Up because treat is shown.
+                    unitFrame.name:ClearAllPoints();
+                    if ReubinsNameplates_settings.Portrait then
+                        unitFrame.name:SetPoint("bottom", nameplate.unitFrame.healthbar, "top", -1, 18);
+                    else
+                        unitFrame.name:SetPoint("bottom", nameplate.unitFrame.healthbar, "top", 8, 18);
+                    end
+
+                    percentage:SetShown(ReubinsNameplates_settings.ThreatPercentage);
+                else
+                    percentage:Hide();
+
+                    -- Shifting unit's name Down because treat is hidden.
+                    unitFrame.name:ClearAllPoints();
+                    if ReubinsNameplates_settings.Portrait then
+                        unitFrame.name:SetPoint("bottom", nameplate.unitFrame.healthbar, "top", -1, 12);
+                    else
+                        unitFrame.name:SetPoint("bottom", nameplate.unitFrame.healthbar, "top", 8, 8);
+                    end
+                end
+            else
+                unitFrame.threat.percentage:Hide();
+            end
+        end
+    end
+end
